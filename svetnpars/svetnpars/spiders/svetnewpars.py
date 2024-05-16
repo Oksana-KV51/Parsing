2e8f59f6f25605963c0ee2206c2c7693a7ff3e40
Oksana-KV51 <12345-70@mail.ru>
2024-05-16 19:07:33 +0400

    add new Scrap SVET

---
+++ svetnpars/svetnpars/spiders/svetnewpars.py
@@ -0,0 +1,22 @@
+import scrapy
+
+
+class SvetnewparsSpider(scrapy.Spider):
+    name = "svetnewpars"
+    allowed_domains = ["https://divan.ru"]
+    start_urls = ["https://www.divan.ru/tolyatti/category/svet"]
+
+    def parse(self, response):
+        svets = response.css('div._Ud0k')
+        # Настраиваем работу с каждым отдельным диваном в списке
+        for svet in svets:
+            yield {
+                # Ссылки и теги получаем с помощью консоли на сайте
+                # Создаём словарик названий, используем поиск по диву, а внутри дива — по тегу span
+                'name': svet.css('div.lsooF span::text').get(),
+                # Создаём словарик цен, используем поиск по диву, а внутри дива — по тегу span
+                'price': svet.css('div.q5Uds span::text').get(),
+                # Создаём словарик ссылок, используем поиск по тегу "a", а внутри тега — по атрибуту
+                # Атрибуты — это настройки тегов
+                'url': svet.css('a').attrib['href']
+            }
